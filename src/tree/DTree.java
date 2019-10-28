324502edd67be562ece6e8b1d10fb8785f30510e
olegbrz <43888615+olegbrz@users.noreply.github.com>
2019-10-28 11:33:40 +0100

    DTree class created

---
+++ src/tree/DTree.java
@@ -0,0 +1,29 @@
+package tree;
+
+public class DTree<E> implements BinaryTree<E> {
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E root() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BinaryTree<E> left() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BinaryTree<E> right() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
